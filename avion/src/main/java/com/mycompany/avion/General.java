e96799d0c6d1bd75155a1957efc50e1809ffbea5
David <David@David>
2019-03-25 19:34:53 -0500

    Clase general

--- avion/src/main/java/com/mycompany/avion/General.java
+++ avion/src/main/java/com/mycompany/avion/General.java
@@ -11,4 +11,40 @@ package com.mycompany.avion;
  */
 public class General {
     
+    private String ubicacion;
+    private String estado;
+    Persona per;
+
+    public General(String ubicacion, String estado, Persona per) {
+        this.ubicacion = ubicacion;
+        this.estado = estado;
+        this.per = per;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Persona getPer() {
+        return per;
+    }
+
+    public void setPer(Persona per) {
+        this.per = per;
+    }
+    
+    
+    
 }
